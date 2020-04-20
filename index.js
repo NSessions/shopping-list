7d7fc7b2d5a7ba4381eeda002e729e38e88e2333
Nathan Sessions <nathan.m.sessions@gmail.com>
2020-04-20 13:39:06 -0600

    Made it possible to add, check and delete items. App doesn't work until user enters text. Unsure how to fix.

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
-$(function()){
-    $('button [type="submit"]').submit(event => {
-
+$(function shoppingListItems(){
+    $('#js-shopping-list-form').submit(event => {
         event.preventDefault();
         
+        let shoppingListItem = $('#shopping-list-entry').val();
+
+        $('.shopping-list').append(
+            `<li>
+            <span class="shopping-item">${shoppingListItem}</span>
+            <div class="shopping-item-controls">
+            <button class="shopping-item-toggle">
+                <span class="button-label">check</span>
+            </button>
+            <button class="shopping-item-delete">
+                <span class="button-label">delete</span>
+            </button>
+            </div>
+        </li>`);
+
+        $('.shopping-list').on('click', '.shopping-item-toggle', function(event){
+            $(event.currentTarget).closest('li').find('.shopping-item').toggleClass('shopping-item__checked');
+        });
         
-
+        $('.shopping-list').on('click', '.shopping-item-delete', function(event) {
+            $(event.currentTarget).closest("li").remove();
+        });
     });
-
+        
 });
